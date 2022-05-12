a6ccbd3df27bcb31b29519fb9021b2c573265022
flaviotn595@gmail.com <flaviotn595@gmail.com>
2022-05-11 22:27:06 -0300

    atualização

--- kurumi.js
+++ kurumi.js
@@ -375,7 +375,7 @@ case 'attp': case 'ttp': {
          }
          break
     case 'emoji2': {
-	    if (!text) throw `Example : ${prefix + command} 😅`
+	    if (!text) throw `Exemplo : ${prefix + command} 😅`
 		let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(text)}`)
 		for (let res of anu.results) {
 		    let encmedia = await kurumi.sendImageAsSticker(m.chat, res.url, m, { packname: global.packname, author: global.author, categories: res.tags })
