7de52b3242e7569cb965a885fed5879c4ad4edc5
flaviotn595@gmail.com <flaviotn595@gmail.com>
2022-05-22 19:41:37 -0300

    atualização

--- kurumi.js
+++ kurumi.js
@@ -425,7 +425,7 @@ if (!m.isGroup) return m.reply(mess.group)
 if (!isBotAdmins) return m.reply(mess.botAdmin)
 if (!isAdmins && !isCreator) return m.reply(mess.admin)
 let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-await kurumi.groupParticipantsUpdate(m.chat, [users], 'remove').then((res) => m.reply(`Usuario removido com sucesso por ordens do admin *${pushname}*`)).catch((err) => m.reply(jsonformat(err)))
+await kurumi.groupParticipantsUpdate(m.chat, [users], 'remove').then((res) => m.reply(`Usuario removido com sucesso por ordens do admin *${pushname}*`)).catch((err) => m.reply('Nao foi possível marquei alguém'))
 }
 break
 case 'add': {
